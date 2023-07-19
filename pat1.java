8950b90fe5559977c90018d6cdf14c2a91e623e3
Shivapoorani <112071110+Shivapoorani@users.noreply.github.com>
2023-07-19 22:55:38 +0530

    Update pat1.java

--- pat1.java
+++ pat1.java
@@ -1,13 +1,13 @@
 import java.util.*;
 class pat1{
-public static void main(String[] args){
+public static void main(String args[]){
 Scanner sc = new Scanner(System.in);
 System.out.println("Enter the Rows: ");
 int rows = sc.nextInt();
 int coef = 1;
 for(int i = 0; i < rows; i++) {
 for(int space = 1; space < rows - i; ++space) {
-System.out.print("  ");
+System.out.println("  ");
 }
 for(int j = 0; j <= i; j++) {
 if (j == 0 || i == 0){
@@ -16,7 +16,7 @@ coef = 1;
 else{
 coef = coef * (i - j + 1) / j;
 }
-System.out.printf("%4d", coef);
+System.out.println("%4d", coef);
 }
 System.out.println();
 }
