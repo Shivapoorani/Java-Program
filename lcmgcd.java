f016c041a4b89adf9d56cd9c8a1898ce43019bdb
Shivapoorani <112071110+Shivapoorani@users.noreply.github.com>
2023-07-19 22:56:11 +0530

    Update lcmgcd.java

--- lcmgcd.java
+++ lcmgcd.java
@@ -1,30 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
 class lcmgcd{
-public static int gcd(int a, int b){
-if (b == 0) return a;
-return gcd(b, a % b);
+public static void main(String args[]){
+int t;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter A: ");
+int a=s.nextInt();
+System.out.println("Enter B: ");
+int b=s.nextInt();
+int atemp = a;
+int btemp = b;
+while(btemp != 0){
+t=btemp;
+btemp=atemp%btemp;
+atemp= t;
 }
-public static int lcm(int a, int b){
-return a * (b / gcd(a, b));
-}
-public static void main(String[] args){
-Scanner sc = new Scanner(System.in);
-System.out.print("N Value : ");
-int n = sc.nextInt();
-int[] arr = new int[n];
-System.out.print("Enter the " + n + " integers: ");
-for (int i = 0; i < n; i++){
-arr[i] = sc.nextInt();
-}
-int lcm = arr[0];
-for (int i = 1; i < n; i++){
-lcm = lcm(lcm, arr[i]);
-}
-int gcd = arr[0];
-for (int i = 1; i < n; i++){
-gcd = gcd(gcd, arr[i]);
-}
-System.out.println("LCM: " + lcm);
-System.out.println("GCD: " + gcd);
+int gcd = atemp;
+int lcm=(a*b)/gcd;
+System.out.println("LCM: "+lcm);
+System.out.println("GCD: "+gcd);
 }
 }
